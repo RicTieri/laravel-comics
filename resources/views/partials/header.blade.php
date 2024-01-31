5ec674eca373fc0948093ce6aaa54483e4f0c1b3
RicTieri <tieri.riccardo@gmail.com>
2024-01-31 18:57:47 +0100

    added dropdown to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,60 +1,66 @@
 <?php 
 
 $links = [
-[
-'title'=> 'characters',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'comics',
-'link'=> '#',
-'isActive'=> true
-],
-[
-'title'=> 'movies',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'tv',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'games',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'collectibles',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'videos',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'fans',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'news',
-'link'=> '#',
-'isActive'=> false
-],
-[
-'title'=> 'shop',
-'link'=> '#',
-'isActive'=> false
-],
+    [
+    'title'=> 'characters',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'comics',
+    'link'=> '#',
+    'isActive'=> true
+    ],
+    [
+    'title'=> 'movies',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'tv',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'games',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'collectibles',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'videos',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'fans',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+    'title'=> 'news',
+    'link'=> '#',
+    'isActive'=> false
+    ],
+    [
+        'title' => 'shop',
+        'link' => '#',
+        'isActive' => false,
+        'dropdown' => [
+            ['title' => 'shop1', 'link' => '#'],
+            ['title' => 'shop2', 'link' => '#'],
+            ['title' => 'shop3', 'link' => '#'],
+        ],
+    ],
 ]
 ?>
+
 <header>
-  <nav class="navbar bg-body-tertiary navbar-expand-lg">
+  <nav class="navbar container navbar-expand-lg">
     <div class="container justify-content-between">
       <a class="navbar-brand" href="#">
         <img src="{{Vite::asset('resources/assets/img/dc-logo.png')}}" alt="Dc logo">
@@ -62,13 +68,28 @@ $links = [
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           @foreach ($links as $link)
+          @if(!isset($link['dropdown']))
           <li class="nav-item text-uppercase fw-bolder">
-            <a class="nav-link active" aria-current="page" href="#">{{$link['title']}}</a>
+            <a class="nav-link active" aria-current="page" href="">{{$link['title']}}</a>
           </li>
+          @else
+            <li class="nav-item text-uppercase fw-bolder dropdown">
+             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{$link['title']}}</a>
+             <ul class="dropdown-menu">
+               @foreach ($link['dropdown'] as $dropdown)
+               <li><a class="dropdown-item" href="{{$dropdown['link']}}">{{$dropdown['title']}}</a></li>
+               @endforeach
+              </ul>
+            </li>
+          @endif
           @endforeach
         </ul>
       </div>
     </div>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
   </nav>
-
 </header>
+
